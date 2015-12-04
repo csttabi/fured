cb2ed627325843b68aa19d5ff3e2e4f664cdea88
unknown <csimatibor@freemail.hu>
2015-12-04 09:03:08 +0100

    message

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Starter Template for Bootstrap 3.3.5</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!--[if IE]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<style>
+    body {
+/*        background: url(images/3-bed-ideas.png) no-repeat center center fixed;*/
+/*        background-color: #fbfaf9;*/
+    }    
+    
+    .box {
+/*        margin: 20px 800px;*/
+        width: 100%;
+        height: 300px;
+        background-color: #ddaf54;
+        box-shadow: inset 0 0 10px #000000;
+    }
+    .box_bal {
+/*        margin: 20px 800px;*/
+        width: 300px;
+        height: 260px;
+        background-color: #5fdd54;
+        box-shadow: inset 0 0 10px #000000;
+        float: left;
+    }
+    input{
+/*        float: left;*/
+    }
+</style>
+
+<body>
+
+<!--
+<img class="box" src="images/3-bed-ideas.png" alt="">
+<img class="box_bal" src="images/3-bed-ideas.png" alt="">
+-->
+<div class="box"></div>
+<div class="box_bal"></div>
+
+<input type="text" placeholder="Text in field">
+   
+
+
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js "></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js "></script>
+</body>
+
+</html>
